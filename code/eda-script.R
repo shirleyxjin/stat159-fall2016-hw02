d7aa1af684bb93aaca18737697f44ae6b75193c6
Shirley Jin <x.jin@berkeley.edu>
2016-10-07 21:16:41 -0700

    changes

--- code/eda-script.R
+++ code/eda-script.R
@@ -2,22 +2,22 @@ advertising <- read.csv("http://www-bcf.usc.edu/%7Egareth/ISL/Advertising.csv")
 
 advertising <- advertising[-1]
 
-sink(file = "data/eda-output.txt")
+sink(file = "../data/eda-output.txt")
 summary(advertising)
 sink()
 
-pdf(filename = "images/histogram-tv.pdf")
+pdf(filename = "../images/histogram-tv.pdf")
 hist(advertising$TV, main="Histogram of TV Budgets",xlab= "TV",ylab="Frequency")
 dev.off
 
-png(filename = "images/histogram-tv.png")
+png(filename = "../images/histogram-tv.png")
 hist(advertising$TV, main="Histogram of TV Budgets",xlab= "TV",ylab="Frequency")
 dev.off
 
-pdf(filename = "images/histogram-sales.pdf")
+pdf(filename = "../images/histogram-sales.pdf")
 hist(advertising$TV, main="Histogram of Sales",xlab="Sales",ylab="Frequency")
 dev.off
 
-png(filename = "images/histogram-sales.png")
+png(filename = "../images/histogram-sales.png")
 hist(advertising$TV, main="Histogram of Sales",xlab="Sales",ylab="Frequency")
 dev.off
